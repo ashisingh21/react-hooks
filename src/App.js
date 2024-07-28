6b8746c0ff121be77d320a063e2a1ef859147700
ashi <ashi888032@gmail.com>
2024-07-28 11:28:56 +0530

    added all files and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UseMemo from "./pages/UseMemo";
+import UseReducer from "./pages/UseReducer";
+import UseCallback from "./pages/UseCallback";
+import UseRef from "./pages/UseRef";
+import Home from "./pages/Home";
+import UseContext from "./pages/UseContext";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/useRef' element={<UseRef />} />
+
+          <Route path='/useMemo' element={<UseMemo />} />
+
+          <Route path='/useReducer' element={<UseReducer />} />
+          <Route path='/useContext' element={<UseContext />} />
+          <Route path='/useCallback' element={<UseCallback />} />
+
+          <Route path='/useRef' element={<useRef />} />
+        </Routes>
+      </BrowserRouter>
+
+
+    </>
   );
 }
 
