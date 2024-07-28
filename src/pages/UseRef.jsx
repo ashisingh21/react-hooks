aef4d6fe534cb45a8e7fe1be9d17a88a9e6ec0d4
ashi <ashi888032@gmail.com>
2024-07-28 11:45:28 +0530

    added use ref hook

--- src/pages/UseRef.jsx
+++ src/pages/UseRef.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const UseRef = () => {
+
+    const button = useRef();
+    const box = useRef();
+
+    const toggleColor = () => {
+        if (box.current.style.backgroundColor === "red") {
+            box.current.style.backgroundColor = "blue"
+        } else {
+            box.current.style.backgroundColor = "red"
+        }
+
+
+
+    }
     return (
-        <div>UseRef</div>
+        <div style={{ margin: "40px" }}>
+            <h2>Use Ref Hook</h2>
+
+            <div ref={box} style={{ border: "1px solid black", width: "300px", height: "300px" }} className='changeMyColor'></div>
+
+            <button style={{ cursor: "pointer", marginTop: "20px", color: "white", backgroundColor: "black", padding: "10px", borderRadius: "5px" }} onClick={toggleColor} ref={button}>Toggle Box Color</button>
+
+        </div>
     )
 }
 
